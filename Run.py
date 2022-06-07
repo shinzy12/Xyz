15416e1ce9e0805241e9b5cd920ad768843ae894
shinzy12 <91730481+shinzy12@users.noreply.github.com>
2022-06-07 18:21:50 +0700

    Delete Run.py

--- Run.py
+++
@@ -1,8 +0,0 @@
-GNU nano 6.2                 run01.py
-#----------------------------------------
-# Compile : Alvino
-# Facebook : legend.alvino
-# Github  : https://github.com/Al-Vino
-#----------------------------------------
-import marshal
-exec(marshal.loads(b'\xe3\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00>
